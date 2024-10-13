400b5d3fc2f21e4b258dd6a5c0992cb14022d806
Ali Hussain <ali@hummingbird.co>
2024-10-13 11:21:26 -0700

    nvim

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -81,14 +81,4 @@ return {
       -- configurations go here
     },
   },
-  {
-    'goolord/alpha-nvim',
-    dependencies = {
-      'echasnovski/mini.icons',
-      'nvim-lua/plenary.nvim',
-    },
-    config = function()
-      require('alpha').setup(require('alpha.themes.theta').config)
-    end,
-  },
 }
